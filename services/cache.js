edbff58e72f128a0af7be4ebfb982cfc29200cb1
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2024-05-20 11:16:57 -0400

    chore: Update Google API credentials and MongoDB connection string

---
+++ services/cache.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+const redisURL = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisURL);
+client.get = util.promisify(client.get);
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function (params) {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
+  const cacheValue = await client.get(key);
+  if (cacheValue) {
+    console.log(cacheValue);
+  }
+
+  const result = await exec.apply(this, arguments);
+  client.set(key, JSON.stringify(result));
+  return result;
+};
